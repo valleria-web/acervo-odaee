b0e08564a4d8e26acfebdd534c41926c4f9eaad1
¨vdsr¨ <valleria@gmail.com>
2023-04-20 23:10:25 +0200

    ¨badgesss¨

--- components/Post.js
+++ components/Post.js
@@ -21,21 +21,24 @@ export default function Post({ post }) {
           <div className="small text-muted">{formattedDate}</div>
           <p className="card-text">{post.frontmatter.author}</p>
           <h2 className="card-title">{post.frontmatter.title}</h2>
-          
+
+        
           <p className="card-text">{post.frontmatter.summary}</p>
+
           <div>
             {post.frontmatter.tags.map((tag) => {
               const slug = slugify(tag)
               return (
-                <span class="badge rounded-pill bg-light text-dark">
                 <Link key={tag} href={`/tag/${slug}`}>
                   <a className="btn">
                     <h6 className="post-title">#{tag}</h6>
                   </a>
-                </Link></span>
+                </Link>
               )
             })}
           </div>
+
+          
           <Link href={`/blog/${post.slug}`}>
             <a className="btn btn-primary">Leer más</a>
           </Link>
