8ba351309c0fa1763b5bbb30cb17b85aead46461
¨vdsr¨ <valleria@gmail.com>
2023-05-09 20:00:41 +0200

    ¨sat¨

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,18 @@
 export default function Footer() {
-    return <footer className="py-5 bg-dark">
-    <div className="container"><p className="m-0 text-center text-white">Copyright &copy; ODAEE</p></div>
-    </footer>
+    const handleClick = () => {
+        window.open("https://ordinals.com/sat/458201456508804", "_blank");
+    };
+
+    return (
+        <footer className="py-5 bg-dark">
+            <div className="container">
+                <p className="m-0 text-center text-white" 
+                   onClick={handleClick}
+                   style={{ cursor: "pointer" }}
+                >
+                    Copyright &copy; <span style={{ textDecoration: "underline" }}>ODAEE</span>
+                </p>
+            </div>
+        </footer>
+    );
 }
